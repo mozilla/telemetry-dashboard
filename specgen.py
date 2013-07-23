d352fcc894990425c3a266774eaf56ddfbb113b3
lonnen <chris.lonnen@gmail.com>
2013-07-23 14:59:02 -0700

    pep8 for everyone

--- specgen.py
+++ specgen.py
@@ -2,19 +2,20 @@ import histogram_tools
 import sys
 import json
 
+
 def get(filename):
     ret = {}
     for h in histogram_tools.from_file(filename):
         try:
             ret[h.name()] = h.ranges()
         except:
-            sys.stderr.write("Could not figure out bucket range for %s\n" % h.name())
+            sys.stderr.write("Could not figure out bucket range for %s\n"
+                             % h.name())
     return ret
 
-"""
-returns a map name->bucket_range
-"""
+
 def mapping(filename):
+    "returns a map name->bucket_range"
     histogram_specs = {}
     for name, ranges in get(filename).iteritems():
             buckets = map(str, ranges)
@@ -24,8 +25,6 @@ def mapping(filename):
             histogram_specs[name] = (bucket2index, ranges)
     return histogram_specs
 
-"""
-Generate a datafile we can use from jydoop
-"""
+"""Generate a datafile we can use from jydoop"""
 if __name__ == '__main__':
     print json.dumps(mapping(sys.argv[1]))
