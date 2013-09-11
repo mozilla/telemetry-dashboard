b837591bb1dab5966dd33e182145d5440649c1fc
Jonas Finnemann Jensen <jopsen@gmail.com>
2013-09-11 19:55:23 +0200

    Simplified aggregation of statistics

--- dashboard.py
+++ dashboard.py
@@ -56,22 +56,14 @@ def map(uid, line, context):
         else:
             bucket2index = bucket2index[0]
 
+        # most buckets contain 0s, so preallocation is a significant win
+        outarray = [0] * (len(bucket2index) + 5)
+
         error = False
         if h_values is None:
             msg = "h_values is None in map"
             print >> sys.stderr, msg
             continue
-        
-        stat_slots_needed = 0
-        if (('sum_squares_hi' in h_values and 'sum_squares_lo' in h_values) or
-           ('log_sum' in h_values and 'log_sum_squares' in h_values)):
-            stat_slots_needed = 2
-        if not stat_slots_needed:
-            print >> sys.stderr, "Skipping this due to statistics"
-            continue
-
-        # most buckets contain 0s, so preallocation is a significant win
-        outarray = [0] * (len(bucket2index) + 2 + stat_slots_needed)
 
         values = h_values.get('values', None)
         if values is None:
@@ -89,18 +81,24 @@ def map(uid, line, context):
             print >> sys.stderr, msg
             continue
 
-        # TODO log_sum and log_sum_squares or sum_squares_hi/lo
         histogram_sum = h_values.get('sum', None)
         if histogram_sum is None:
             msg = "histogram_sum is None in map"
             print >> sys.stderr, msg
             continue
+        # if statistics isn't available we just leave the two slots as zeroes
         if 'sum_squares_hi' in h_values and 'sum_squares_lo' in h_values:
-            outarray[-4] = h_values.get('sum_squares_hi', None)
-            outarray[-3] = h_values.get('sum_squares_lo', None)
+            outarray[-4] = h_values.get('sum_squares_hi', 0)
+            outarray[-3] = h_values.get('sum_squares_lo', 0)
         elif 'log_sum' in h_values and 'log_sum_squares' in h_values:
-            outarray[-4] = h_values.get('log_sum', None)
-            outarray[-3] = h_values.get('log_sum_squares', None)
+            outarray[-4] = h_values.get('log_sum', 0)
+            outarray[-3] = h_values.get('log_sum_squares', 0)
+        if not isinstance(outarray[-4], (int, long, float)):
+            print >> sys.stderr, "=======> 4 isn't numeric!!"
+            continue
+        if not isinstance(outarray[-3], (int, long, float)):
+            print >> sys.stderr, "=======> 3 isn't numeric!!"
+            continue
         outarray[-2] = histogram_sum
         outarray[-1] = 1        # count
         try:
@@ -149,8 +147,8 @@ def reduce(key, values, context):
                 if T is float:
                     if i is len(histogram) - 3 or i is len(histogram) - 4:
                         continue # allow elements of stats to be floats
-                msg = ("discarding %s. contrained malformed type(s): %s" %
-                       ('/'.join(filter_path), set(malformed_data)))
+                msg = ("discarding %s. contrained malformed type: %s" %
+                       ('/'.join(filter_path), T))
                 print >> sys.stderr, msg
                 return
         out_values["/".join(filter_path)] = histogram
