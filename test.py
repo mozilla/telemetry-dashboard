d8ee3e0f277c885b20d6076ea3d8391aa2361311
Jonas Finnemann Jensen <jopsen@gmail.com>
2013-09-24 15:02:02 +0200

    Port map/reduce job for histograms, but the framework is probably too naive... no combine :(

--- test.py
+++ test.py
@@ -1,5 +1,56 @@
+
+# Auxiliary method to write log messages
+def log(msg):
+    if verbose:
+        print >> sys.stderr, msg
+
 def map(key, dims, value, context):
-    context.write("/".join(dims), 1)
+    # Unpack dimensions
+    reason, appName, channel, version, buildId, submissionDate = dim
+
+    # Get the major version
+    majorVersion = version.split('.')[0]
+
+    # Get the build date, ignore the rest of the buildId
+    buildDate = buildId[:8]
+
+    # Load JSON payload
+    payload = json.loads(value)
+
+    # Get OS, osVersion and architecture information
+    try:
+        info = payload['info']
+        OS = info['OS']
+        osVersion = str(info['version'])
+        arch = info['arch']
+    except (KeyError, IndexError, UnicodeEncodeError):
+        log("error while unpacking the payload")
+        return
+
+    # todo combine OS + osVersion + santize on crazy platforms like linux to
+    #      reduce pointless choices
+    if OS == "Linux":
+        osVersion = osVersion[:3]
+
+    # Create filter path
+    filterPath = (buildDate, reason, appName, OS, osVersion, arch)
+
+    # For each histogram
+    for hgramName, hgramValues in payload.get('histograms', {}).iteritems():
+        context.write((channel, majorVersion, hgramName),
+                      {filterPath: hgramValues})
+
+def commonCombine(values):
+    output = {}
+    for d in values:
+        for filterPath, hgramValues in d.iteritems():
+            existing = output.get(filterPath, None)
+            if existing is None:
+                output[filter_path] = hgramValues
+                continue
+            for y in xrange(0, len(hgramValues)):
+                existing[y] += (hgramValues[y] or 0)
+    return output
 
 def reduce(key, values, context):
-    context.write(key, sum(values))
+    context.write(key, commonCombine(values))
