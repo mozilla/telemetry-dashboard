mozilla/telemetry-dashboard

commit 71d6fbe8751fed50d329df8d5ad575135eaefb6e

Rebase chart-fixes onto master.

src/dashboard.js
@@ -757,9 +757,12 @@ function renderHistogramGraph(hgram) {
       tooltipTemplate: function(valuesObject) { return tooltipLabels[valuesObject.label] || valuesObject.label; },
     });
     
-    // Assign random colors to make it easy to differentiate between bars
+    // Assign fixed pseudorandom colors to make it easy to differentiate between bars
+    var seed = 0.2;
     gCurrentHistogramPlot.datasets[0].bars.forEach(function(bar) {
-      bar.fillColor = "hsla(" + Math.floor(Math.random() * 256) + ", 80%, 70%, 0.8)";
+      seed = Math.sin(seed) * 10000;
+      var hue = Math.floor((seed - Math.floor(seed)) * 256);
+      bar.fillColor = "hsla(" + hue + ", 80%, 70%, 0.8)";
     });
     gCurrentHistogramPlot.update();
   });
@@ -930,8 +933,13 @@ function update(hgramEvos) {
     }
 
     // Create new series with updated fields for each entry
+    var futureCutoff = moment().add(1, "years").valueOf();
     series = $.map(series, function(entry, i) {
       // Update the bounds properly
+      if (point.x >= futureCutoff) {
+        console.log("Bad point; timestamp is far into the future: " + point.x);
+        continue;
+      }
       entry.values.forEach(function(point) {
         if (start === null || point.x < start) {
           start = point.x;

commit 7ad3d59b9ad513ebf9a93881b1a659a37fc1b592

Fix bad rebase caused by 71d6fbe8751fed50d329df8d5ad575135eaefb6e.

src/dashboard.js
@@ -936,16 +936,16 @@ function update(hgramEvos) {
     var futureCutoff = moment().add(1, "years").valueOf();
     series = $.map(series, function(entry, i) {
       // Update the bounds properly
-      if (point.x >= futureCutoff) {
-        console.log("Bad point; timestamp is far into the future: " + point.x);
-        continue;
-      }
       entry.values.forEach(function(point) {
-        if (start === null || point.x < start) {
-          start = point.x;
-        }
-        if (end === null || point.x > end) {
-          end = point.x;
+        if (point.x >= futureCutoff) {
+          console.log("Bad point; timestamp is far into the future: " + point.x);
+        } else {
+          if (start === null || point.x < start) {
+            start = point.x;
+          }
+          if (end === null || point.x > end) {
+            end = point.x;
+          }
         }
       });
       