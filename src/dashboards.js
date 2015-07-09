361c37d0ad1c9be07fdf44dbfeafa71b59b705c5
Anthony Zhang <azhang@mozilla.com>
2015-07-09 13:08:19 -0400

    Also allow searching for the names without spaces or underscores.

--- src/dashboards.js
+++ src/dashboards.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
       options.filterCallback = function(element, query) {
         var value = $(element).find("label").text().toLowerCase();
         query = query.toLowerCase();
-        return value.indexOf(query) >= 0 || value.replace(/_/g, " ").indexOf(query) >= 0;
+        return value.indexOf(query) >= 0 || value.replace(/_/g, " ").indexOf(query) >= 0 || value.indexOf(query.replace(/[ _]/g, "")) >= 0;
       };
     }
     if ($this.attr("title") !== undefined) {
