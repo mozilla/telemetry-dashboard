c1b5b81733ebebc9e909a1f24e61e4486cf8138d
Anthony Zhang <azhang@mozilla.com>
2015-08-06 14:28:57 -0400

    Use short names for the OS selector tooltip text.

--- new-pipeline/src/dashboards.js
+++ new-pipeline/src/dashboards.js
@@ -53,6 +53,9 @@ $(document).ready(function() {
           }
         }
       }
+      options.buttonTitle = function(options, select) {
+        return getHumanReadableOptions("os", compressOSs()).map(function(option) { return option[1]; }).join(", ");
+      }
     }
     
     if ($this.attr("title") !== undefined) {
