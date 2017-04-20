cdad954cf45aa4fa797254c6610d39458ed3eeb7
Chris H-C <chutten@mozilla.com>
2017-04-20 13:19:27 -0400

    bug 1355541 - Use the new ESR normalized-channel instead of Other.
    bug 1352091 split 'esr' out as separate from 'Other' starting on 2017-04-19.
    So let's use 'Other' before that date and 'esr' from then on.

--- crashes/thresholds.js
+++ crashes/thresholds.js
@@ -43,7 +43,7 @@ const THRESHOLDS = {
     [CRASH_NAMES.MCS]: {lo: 3.03, hi: 4.02},
     [CRASH_NAMES.P]: {lo: 5.99, hi: 9.74},
   },
-  "Other": {
+  "esr": {
     [CRASH_NAMES.M]: {lo: 2.17, hi: 4.07},
     [CRASH_NAMES.MC]: {lo: 3.95, hi: 6.89},
     [CRASH_NAMES.CS]: {lo: 0.81, hi: 0.88},
