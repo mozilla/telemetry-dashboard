0f45c43de77987f74ba44e66a97892d3f8980394
Sylvia23 <mittalsylvia23@gmail.com>
2018-03-15 05:23:55 +0530

    Fix #459 : Stats tab doesn't display if channel is set to any

--- probe-dictionary/explore.js
+++ probe-dictionary/explore.js
@@ -227,8 +227,8 @@ function showSearchOnlyFilters(show) {
     }
 
     var channel = $("#select_channel").val();
-    if (!["release", "beta", "nightly", "any"].includes(channel)) {
-      $("#select_channel").val("any");
+    if (!["release", "beta", "nightly"].includes(channel)) {
+      $("#select_channel").val("release");
     }
   }
 }
