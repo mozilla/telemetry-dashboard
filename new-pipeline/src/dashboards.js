ca1842d9afe66ca5080948a753f4e4dc7518a469
Erin Comerford <erin.comerford3@gmail.com>
2018-05-17 14:37:54 -0700

    update

--- new-pipeline/src/dashboards.js
+++ new-pipeline/src/dashboards.js
@@ -1084,7 +1084,7 @@ function getDescriptionLink(metric, channel, description) {
   return link;
 }
 
-function showUseCounterLink(metric, channel, description) {
+function getUseCounterLink(metric, channel, description) {
   //Show correct use counter link based on group selection.
   var metricUrl = buildDictionaryURL(metric, channel, description);
   if (metric.startsWith("USE_COUNTER") !== true) {
@@ -1100,6 +1100,6 @@ function showUseCounterLink(metric, channel, description) {
       target: "_blank",
     });
     useCounterLink.text("View in use counter dashboard.");
+    return useCounterLink;
   }
-  return useCounterLink;
 }
