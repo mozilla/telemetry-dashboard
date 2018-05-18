cd4d2d342e2af208eaccb0a96952a1324bef81c0
Erin Comerford <erin.comerford3@gmail.com>
2018-05-18 09:47:29 -0700

    test measure v metric.

--- new-pipeline/src/dashboards.js
+++ new-pipeline/src/dashboards.js
@@ -1085,12 +1085,13 @@ function getDescriptionLink(metric, channel, description) {
 }
 
 function getUseCounterLink(metric, channel, description) {
-  if (metric.startsWith("USE_COUNTER") !== true) {
+  var measure = metric;
+  if (measure.startsWith("USE_COUNTER") !== true) {
     return null;
   }
   else {
-    metricSplit = metric.split("_");
-    var group = metricSplit[2];
+    measureSplit = measure.split("_");
+    var group = measureSplit[2];
     var useCounterLink = $("<a>", {
       href: "http://georgf.github.io/usecounters/#kind=page&group=" + group + "&channel=beta&version=60",
       target: "_blank",
