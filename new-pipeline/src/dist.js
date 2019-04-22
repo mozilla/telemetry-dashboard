mozilla/telemetry-dashboard

commit 5b10b57ab16b119ca37b9d4c0f4f177eb9d01f9d

Implemented option to Hide the "spill" bucket from categorical histograms behind an advanced setting (#578)
* implemented the option to Hide the "spill" bucket from categorical histograms behind an advanced setting

* Inlcude spill label modified and code formating done

* Revert "Inlcude spill label modified and code formating done"

This reverts commit 124d75e2ca95690c3d880f3f2c2cd50b534c69fc.

* Revised - Inlcude spill label modified and code formating done

* Code Formatting changes added

* Formatting added

* Required Formatting added

new-pipeline/src/dist.js
@@ -80,6 +80,9 @@ $(function () {
     $("input[name=sanitize-toggle]")
       .prop("checked", gInitialPageState.sanitize !== 0)
       .trigger("change");
+      $("input[name=include-spill-toggle]")
+      .prop("checked", gInitialPageState.include_spill > 0)
+      .trigger("change");
 
     updateOptions(function () {
       if (gInitialPageState.product !== null) {
@@ -250,7 +253,7 @@ $(function () {
                 if (getAggregate === undefined) {
                   throw "Could not obtain aggregate function"
                 };
-
+                
                 gCurrentHistogramsList = Object.keys(
                     histogramsMap)
                   .map(function (label) {
@@ -335,7 +338,7 @@ $(function () {
         });
 
       $(
-          "#selected-key1, #selected-key2, #selected-key3, #selected-key4, input[name=table-toggle], input[name=cumulative-toggle], input[name=trim-toggle]"
+          "#selected-key1, #selected-key2, #selected-key3, #selected-key4, input[name=table-toggle], input[name=cumulative-toggle], input[name=trim-toggle], input[name=include-spill-toggle]"
         )
         .change(function (e) {
           if (gCurrentHistogramsList.length > 1) { // Keyed histogram with multiple keys, find the selected keys
@@ -359,12 +362,13 @@ $(function () {
               }
             });
           } else { // Non-keyed histogram or a keyed histogram with only one key
-            var histogramsList = gCurrentHistogramsList;
+            var histogramsList = makenewHistogramListCopy(gCurrentHistogramsList);
           }
           displayHistograms(histogramsList, gCurrentDates,
             $("input[name=table-toggle]").is(':checked'),
             $("input[name=cumulative-toggle]").is(':checked'),
-            $("input[name=trim-toggle]").is(':checked')
+            $("input[name=trim-toggle]").is(':checked'),
+            $("input[name=include-spill-toggle]").is(':checked')
           );
           saveStateToUrlAndCookie();
         });
@@ -788,9 +792,17 @@ function updateDateRange(callback, dates, updatedByUser, shouldUpdateRangebar) {
   }
 }
 
-function displayHistograms(histogramsList, dates, useTable, cumulative, trim) {
+function displayHistograms(histogramsList, dates, useTable, cumulative, trim, includeSpill) {
   cumulative = cumulative === undefined ? false : cumulative;
   trim = trim === undefined ? true : trim;
+  // Hide the 'spill' bucket if present
+  if (!includeSpill) {
+    const spillIndex = histogramsList[0].histograms[0].buckets.indexOf("spill");
+    histogramsList[0].histograms[0].buckets = histogramsList[0].histograms[0].buckets
+                                            .filter((value, index) => index !== spillIndex);
+    histogramsList[0].histograms[0].values = histogramsList[0].histograms[0].values
+                                            .filter((value, index) => index !== spillIndex);
+  }
 
   var minTrimLeft = 0,
     minTrimRight = 0,
@@ -1640,6 +1652,8 @@ function saveStateToUrlAndCookie() {
       .format("YYYY-MM-DD"),
     end_date: moment(picker.endDate)
       .format("YYYY-MM-DD"),
+    include_spill: $("input[name=include-spill-toggle]")
+      .is(":checked") ? 1 : 0
   };
 
   // Save a few unused properties that are used in the evolution dashboard, since state is shared between the two dashboards
@@ -1869,4 +1883,24 @@ function saveStateToUrlAndCookie() {
 
 function isHistogramsListKeyed(histogramsList) {
   return histogramsList.some(entry => entry.title !== "");
+}
+
+function makenewHistogramListCopy(histogramList) {
+ return histogramList.map((item) => {
+    return {
+      ...item,
+      histograms: item.histograms.map((histogram) => {
+        const newHistCopy = Object.create(histogram);
+        newHistCopy.buckets = histogram.buckets.slice(0);
+        newHistCopy.count = histogram.count;
+        newHistCopy.description = histogram.description;
+        newHistCopy.kind = histogram.kind;
+        newHistCopy.measure = histogram.measure;
+        newHistCopy.submissions = histogram.submissions;
+        newHistCopy.sum = histogram.sum;
+        newHistCopy.values = histogram.values.slice(0);
+        return newHistCopy;
+      }),
+    };
+  });
 }

new-pipeline/dist.html
@@ -188,6 +188,10 @@
                                     <label><input name="trim-toggle" type="checkbox"> <b>Trim small buckets</b> </label>
                                     Hide buckets on both sides that don't have enough samples.
                                 </div>
+                                <div>
+                                    <label><input name="include-spill-toggle" type="checkbox"> <b>Include Spill</b> </label>
+                                    Include 'spill' bucket (for categorical histograms)
+                                </div>
                                 <span id="date-range-controls" class="form-group" style="width: 230px; display: inline-block; margin: 0">
                                     <span class="input-prepend input-group">
                                         <span class="add-on input-group-addon"><i class="glyphicon glyphicon-calendar fa fa-calendar"></i></span>

new-pipeline/evo.html
@@ -97,6 +97,10 @@
                                     <label><input name="sanitize-toggle" type="checkbox"> <b>Sanitize data</b> </label>
                                     Hide data points that don't have enough submissions.
                                 </div>
+                                <div>
+                                    <label><input name="include-spill-toggle" type="checkbox"> <b>Include Spill</b> </label>
+                                    Include 'spill' bucket (for categorical histograms)
+                                </div>
                             </form>
                         </div>
                     </div>

new-pipeline/src/evo.js
@@ -109,6 +109,9 @@ $(function () {
     $("input[name=sanitize-toggle]")
       .prop("checked", gInitialPageState.sanitize !== 0)
       .trigger("change");
+    $("input[name=include-spill-toggle]")
+      .prop("checked", gInitialPageState.include_spill > 0)
+      .trigger("change");  
 
     // If advanced settings are not at their defaults, expand the settings pane on load
     if (gInitialPageState.use_submission_date !== 0 ||
@@ -245,7 +248,7 @@ $(function () {
           });
         });
       $(
-          "#measure, input[name=build-time-toggle], input[name=sanitize-toggle], #aggregates, #filter-product, #filter-os, #filter-arch, #filter-process-type"
+          "#measure, input[name=build-time-toggle], input[name=sanitize-toggle], #aggregates, #filter-product, #filter-os, #filter-arch, #filter-process-type, input[name=include-spill-toggle]"
         )
         .change(function (e) {
           var $this = $(this);
@@ -394,8 +397,14 @@ var gLoadedAggregatesFromState = false;
 function updateAggregates(kind, buckets) {
   gCurrentKind = kind;
   let newAggregates;
+  const IncludeSpill = $("input[name=include-spill-toggle]").is(":checked");
+
   if (kind === "enumerated" || kind === "categorical") {
     newAggregates = getHumanReadableBucketOptions(kind, buckets);
+    // Hide spill Option from Aggregates based on advanced settings
+    if (!IncludeSpill) {
+      newAggregates = newAggregates.filter(([key,value]) => value !== "spill");
+    }
     multiselectSetOptions($("#aggregates"), newAggregates, [newAggregates[0][0]]);
   } else if (kind === "boolean" || kind === "flag") {
     newAggregates = getHumanReadableBucketOptions(kind, buckets);
@@ -407,7 +416,6 @@ function updateAggregates(kind, buckets) {
       .multiselect("updateButtonText");
   } else { // `kind` is another histogram kind, or null because we didn't have any data
     newAggregates = gDefaultAggregates.map(entry => [entry[0], entry[1]]);
-
     multiselectSetOptions($("#aggregates"), newAggregates, kDefaultSelectedAggregates);
   }
 
@@ -944,6 +952,8 @@ function saveStateToUrlAndCookie() {
       .is(":checked") ? 1 : 0,
     sanitize: $("input[name=sanitize-toggle]")
       .is(":checked") ? 1 : 0,
+    include_spill: $("input[name=include-spill-toggle]")
+      .is(":checked") ? 1 : 0
   };
 
   // Save a few unused properties that are used in the distribution dashboard, since state is shared between the two dashboards

commit 2f05a907d67fc874e44a60c480a88e76364375ba

Fix index out of bounds when checking spill.
Fixes #606
Fixes #623

Regression from #578

new-pipeline/src/dist.js
@@ -813,7 +813,7 @@ function displayHistograms(histogramsList, dates, useTable, cumulative, trim, in
   cumulative = cumulative === undefined ? false : cumulative;
   trim = trim === undefined ? true : trim;
   // Hide the 'spill' bucket if present
-  if (!includeSpill) {
+  if (!includeSpill && histogramsList.length) {
     const spillIndex = histogramsList[0].histograms[0].buckets.indexOf("spill");
     histogramsList[0].histograms[0].buckets = histogramsList[0].histograms[0].buckets
                                             .filter((value, index) => index !== spillIndex);